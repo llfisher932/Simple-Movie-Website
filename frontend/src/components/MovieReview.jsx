8ad6a64639dfba7d22d52ce9cf9707be3dde9af4
Logan Fisher <llfisher932@gmail.com>
2025-12-08 21:26:09 -0500

    Fixed stars on mobile screen.

--- frontend/src/components/MovieReview.jsx
+++ frontend/src/components/MovieReview.jsx
@@ -6,7 +6,7 @@ export default function MovieReview({ username, number, text }) {
       <div className="flex">
         <h2 className="font-bold">{username}</h2>
 
-        <div className="flex ml-2">
+        <div className="flex ml-2 mr-2 justify-center items-center">
           {/* uses prety much same logic as stars to leave review, but just rended filled stars based on database number, not click */}
           {[1, 2, 3, 4, 5].map((star) => (
             <svg
@@ -14,7 +14,7 @@ export default function MovieReview({ username, number, text }) {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill={star <= number ? "yellow" : "gray"}
-              className="w-6 h-6 ">
+              className="md:w-6 md:h-6 w-5 h-5">
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.947a1 1 0 00.95.69h4.15c.969 0 1.371 1.24.588 1.81l-3.36 2.44a1 1 0 00-.364 1.118l1.287 3.947c.3.921-.755 1.688-1.54 1.118l-3.36-2.44a1 1 0 00-1.175 0l-3.36 2.44c-.784.57-1.838-.197-1.539-1.118l1.286-3.947a1 1 0 00-.364-1.118L2.034 9.374c-.783-.57-.38-1.81.588-1.81h4.15a1 1 0 00.95-.69l1.287-3.947z" />
             </svg>
           ))}
